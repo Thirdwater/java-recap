85980076bc1b37c5d50e14bf6ec7c937b610516e
Thirdwater <irinthirdwater@gmail.com>
2020-09-02 19:09:23 +0700

    Add the Getting Started trail

---
+++ 1-getting-started/HelloWorldApp.java
@@ -0,0 +1,26 @@
+/**
+ * Save java source code in .java files.
+ * Compile java source code into .class bytecode using javac command.
+ * Run java bytecode using java command.
+ *
+ * Or use java-compatible IDE.
+ */
+class HelloWorldApp {
+
+    // Every application must contain a main method.
+    // Use java HelloWorldApp arg1 arg2 ... to supply arguments.
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+
+        /**
+         * Documentation
+         */
+        /*
+         * Delimited comment
+         * (delimiters don't have to span multiple lines)
+         */
+        // To-eol comment
+        // (entire line preceding the // won't be commented)
+    }
+
+}
