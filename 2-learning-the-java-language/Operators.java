9aa9923956c4618034614eea844303922599ef4d
Thirdwater <irinthirdwater@gmail.com>
2020-09-03 20:57:39 +0700

    Add small notes on basic operators

---
+++ 2-learning-the-java-language/Operators.java
@@ -0,0 +1,18 @@
+class Operators {
+
+    void examples () {
+        /*
+         * Increment/Decrement Operators
+         * Prefix version evaluates to the value after the operation.
+         * Postfix version evaluates to the value before the operation.
+         */
+        // Compare types using A instance of B.
+        // null is not an instance of anything.
+        /*
+         * Bit Manipulation
+         * #TODO
+         */
+
+    }
+
+}
