cfa83639b996380d068eb808536c90769429122e
Thirdwater <irinthirdwater@gmail.com>
2020-09-06 22:03:20 +0700

    Modify problem constraint to cover cases with minus sign (but will be too permissive in general).

--- hackerrank/bignumber/primality-test/Solution.java
+++ hackerrank/bignumber/primality-test/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
     public static void main (String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             String input = scanner.nextLine();
-            assert input.length() <= 100;
+            assert input.length() <= 101;
             BigInteger integer = new BigInteger(input);
             // Just need 1 to pass the test cases.
             boolean isProbablyPrime = integer.isProbablePrime(10);
